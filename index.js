b766588d67c2b03a38cf1f8decea43ba9b32d127
Myles Gray <mg@mylesgray.com>
2018-04-09 16:00:39 +0100

    Reverted lib name in code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var moment = require('moment');
 var util = require('util');
-var lightify = require('node-lightify-mg');
+var lightify = require('node-lightify');
 var Service, Characteristic;
 var Lightbulb;
 
